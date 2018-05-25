40d2b53c7ef85fcc4a157b63c7dcec30bcca3f3f
cereceres <jesus.edelcereceres@gmail.com>
2018-05-24 21:39:56 -0600

    the test to update response is fixed

--- controllers/response/put-response.controller.js
+++ controllers/response/put-response.controller.js
@@ -1,11 +1,16 @@
 const Response = require('../../models/response.model');
+const Post = require('../../models/post.model');
 
 module.exports = (req, res, next) => (async() => {
     const query = req.query || req.params.id && { _id:req.params.id } || {};
     const data = req.body || {};
-
+    const { post_id } = data;
     const response = await Response.update(data, query);
 
+    if (!post_id) return res.json(response);
+
+    const responses_count = await Response.count({ post_id });
+    await Post.update({ responses_count }, { post_id });
     res.json(response);
 })()
     .catch(({ message }) => res.status(400).json({ message }));
--- test/big/response/put.test.js
+++ test/big/response/put.test.js
@@ -1,22 +1,31 @@
 const assert = require('assert');
 
 const Response = require('../../../models/response.model');
+const Post = require('../../../models/post.model');
 
 
 describe('test to get response', () => {
     it('should the return the response saved in db', async() => {
+        const res = await Post.create({
+            post_id:'4444',
+            user_id: '1',
+            post_content:'post_content'
+        });
         const response = await Response.create({
-            post_id:'3',
+            post_id:'4',
             user_id: '2',
             response_id: '10',
             response_content:'something here'
         });
-        const { body } = await agent.put(`/api/response/${response.id}`)
+        await agent.put(`/api/response/${response.id}`)
             .send({
+                post_id:'4444',
                 response_content:'something here-updated',
             })
             .expect(200);
         const responseUpdated = await Response.find({ response_id: '10' });
         assert(responseUpdated[0].response_content === 'something here-updated');
+        const posts = await Post.find({ post_id: '4444' });
+        assert(posts[0].responses_count === 1);
     });
 });
