f99b9e13a13d12a8aa2a99e815aebc4144b0488e
cereceres <jesus.edelcereceres@gmail.com>
2018-05-24 23:11:06 -0600

    the protected path are tested

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const jwt = require('./lib/jwt');
 const database = require('./database');
 const userRouter = require('./router/user.router');
 const postRouter = require('./router/post.router');
@@ -8,10 +10,9 @@ const authRouter = require('./router/auth.router');
 
 const PORT = process.env.PORT;
 const app = express();
-
-
 const jsonParser = bodyParser.json();
 
+app.use(jwt());
 app.use(jsonParser);
 app.use('/api', userRouter);
 app.use('/api', postRouter);
